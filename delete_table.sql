5e9bb9c122871e18233ac598ac634a2c2f7a2389
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-19 18:31:46 +0530

    delete table

---
+++ delete_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE student(name VARCHAR(25), age INT, prn VARCHAR(15));
+INSERT INTO student VALUES('vivek', 22, '202207923');
+INSERT INTO student VALUES('rohan', 21, '202208212');
+SELECT * FROM student;
+DROP TABLE student;
